dd0cbc7b15c2d1883a14d58e7fb9ff08cc16d386
Kağan Gündüztepe <kagangunduztepe@gmail.com>
2026-03-16 17:03:57 +0300

    env tweak

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,12 @@
 export const environment = {
-  production: false,
-  rootUrl: 'http://localhost:5005',      // Dosya yolları için (uploads vb.)
-  apiUrl: 'http://localhost:5005/api'   // API istekleri için
+  production: true,
+  rootUrl: 'https://bankapp-app-v2-production.up.railway.app',      // Dosya yolları için (uploads vb.)
+  apiUrl: 'https://bankapp-app-v2-production.up.railway.app/api'   // API istekleri için
 };
+
+
+//export const environment = {
+// production: false,
+//  rootUrl: 'http://localhost:5005',      // Dosya yolları için (uploads vb.)
+//  apiUrl: 'http://localhost:5005/api'   // API istekleri için
+//};
